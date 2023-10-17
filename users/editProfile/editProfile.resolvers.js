5774f1811886af16e3de55a2e13b8344540cf91c
김동욱 <email@example.com>
2023-10-17 21:06:14 +0900

    2023.10.17 edit_profile

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,5 +1,37 @@
+import bcrypt from "bcrypt";
+import client from "../../client";
+
 export default {
   Mutation: {
-    editProfile: () => console.log("hi"),
+    editProfile: async (
+      _,
+      { firstName, lastName, email, password: newPassword }
+    ) => {
+      let uglyPassword = null;
+      if (newPassword) {
+        uglyPassword = await bcrypt.hash(newPassword, 10);
+      }
+      const updateUser = await client.user.update({
+        where: {
+          id: 1,
+        },
+        data: {
+          firstName,
+          lastName,
+          email,
+          ...(uglyPassword && { password: uglyPassword }), // ...(uglyPassword && {password: uglyPassword}) uglyPassword가 눌이면 참이다 뒤에꺼 실행
+        },
+      });
+      if (updateUser.id) {
+        return {
+          ok: true,
+        };
+      } else {
+        return {
+          ok: false,
+          error: "프로필을 업데이트 할 수 없습니다.",
+        };
+      }
+    },
   },
 };
