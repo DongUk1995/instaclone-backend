4767d2c1137e066585c6f062dda7e618b344335b
김동욱 <email@example.com>
2023-10-19 09:18:20 +0900

    2023.10.19

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 import client from "../../client";
 import jwt from "jsonwebtoken";
+import { protectResolver } from "../users.utils";
 
 export default {
   Mutation: {
@@ -9,7 +10,7 @@ export default {
       { firstName, lastName, email, password: newPassword },
       { loggedInUser }
     ) => {
-      console.log(loggedInUser);
+      protectResolver(loggedInUser);
       let uglyPassword = null;
       if (newPassword) {
         uglyPassword = await bcrypt.hash(newPassword, 10);
