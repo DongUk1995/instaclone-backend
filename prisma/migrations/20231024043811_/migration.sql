29ea5f23b40c772159d494b2dd35c38feba3505c
김동욱 <email@example.com>
2023-10-24 13:38:17 +0900

    22

--- prisma/migrations/20231024043811_/migration.sql
+++ prisma/migrations/20231024043811_/migration.sql
@@ -6,6 +6,10 @@ CREATE TABLE "User" (
     "username" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "bio" TEXT,
+    "avatar" TEXT,
+    "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
