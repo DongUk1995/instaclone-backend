b1fd0b3899ef0a97170b6b0024d96619f9a49789
김동욱 <email@example.com>
2023-10-25 22:35:20 +0900

    4.9

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ import { ApolloServer } from "apollo-server";
 import schema from "./schema";
 
 const PORT = process.env.PORT;
-const server = new ApolloServer({ schema });
+const server = new ApolloServer({
+  schema,
+  context: {
+    token:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjk4MjM4NjI2fQ.dGgFGGHb-LPlbTpC5JWJ5H23LcruEoHUSDqX0IvmKls",
+  },
+});
 server
   .listen(PORT)
   .then(() => console.log(`😗server is running on http://localhost:${PORT}/`));
--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -6,7 +6,8 @@ export default {
   Mutation: {
     editProfile: async (
       _,
-      { firstName, lastName, username, email, password: newPassword, token }
+      { firstName, lastName, username, email, password: newPassword },
+      { token }
     ) => {
       const { id } = await jwt.verify(token, process.env.SECRECT_KEY); // 토근 안에 서명된 아이디를 확인 할 수있다. 토큰을 해독한다.
       // const verifiedToken인데 임마를 출력하면 {id:1, iat: xxxx}가 나온다. 즉 오브젝트가 나오기 때문에 우리는 id가 필요하니깐 {id}를 사용해서 id꺼내서 사용하는 것
