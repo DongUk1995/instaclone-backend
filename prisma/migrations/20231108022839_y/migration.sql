693098cc278422c8ffa2ed1157d17cd8b52127f0
김동욱 <email@example.com>
2023-11-08 11:37:12 +0900

    message_Room_create

---
+++ prisma/migrations/20231108022839_y/migration.sql
@@ -0,0 +1,28 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Message` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Room` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `_RoomToUser` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Message" DROP CONSTRAINT "Message_roomId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Message" DROP CONSTRAINT "Message_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_RoomToUser" DROP CONSTRAINT "_RoomToUser_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_RoomToUser" DROP CONSTRAINT "_RoomToUser_B_fkey";
+
+-- DropTable
+DROP TABLE "Message";
+
+-- DropTable
+DROP TABLE "Room";
+
+-- DropTable
+DROP TABLE "_RoomToUser";
